b1b22b338c6adf22f5383693a18440219625c4f1
Estefane Oliveira <epbocin@gmail.com>
2020-10-21 17:35:59 -0300

    WIP

---
+++ recipes_recommendation/dataset.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def foo(dataframe):
+    pd.DataFrame([20, 269.8, 22.0, 32.0, 48.0, 39.0, 27.0, 5.0]], columns = ['id', 'calories', 'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat']) 
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,2 +1,30 @@
-def test_should_map_one_user_to_index():
-    assert True is False
+import pandas as pd
+from recipes_recommendation import dataset
+
+def test_should_explode_nutrition_column():
+    data = [
+        20,
+        "[269.8, 22.0, 32.0, 48.0, 39.0, 27.0, 5.0]"
+    ]
+    df = pd.DataFrame(data, columns = ['id', 'nutrition']) 
+    
+    actual = dataset.extract_features(df)
+    expected = pd.DataFrame([20, 269.8, 22.0, 32.0, 48.0, 39.0, 27.0]], columns = ['id', 'calories', 'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat']) 
+    
+    assert expected == actual
+
+
+def test_should_explode_nutrition_column_ignore_other_columns():
+    data = [
+        20,
+        "bar",
+        "[269.8, 22.0, 32.0, 48.0, 39.0, 1337.0, 5.0]"
+    ]
+    df = pd.DataFrame(data, columns = ['id', "foo",'nutrition']) 
+    
+    actual = dataset.extract_features(df)
+    expected = pd.DataFrame([20, 269.8, 22.0, 32.0, 48.0, 39.0, 1337.0]], columns = ['id', 'calories', 'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat']) 
+    
+    assert expected == actual
+
+
