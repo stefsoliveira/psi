d338305e47a66b420834f142ef6c6b3bae7f4df8
Daniel Schruhl <danielschruhl@gmail.com>
2020-10-22 08:47:24 +0200

    Fix tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,30 +1,34 @@
 import pandas as pd
+import pandas.testing as pdt
+
 from recipes_recommendation import dataset
 
+
 def test_should_explode_nutrition_column():
-    data = [
-        20,
-        "[269.8, 22.0, 32.0, 48.0, 39.0, 27.0, 5.0]"
-    ]
-    df = pd.DataFrame(data, columns = ['id', 'nutrition']) 
-    
+    data = [[20, "[269.8, 22.0, 32.0, 48.0, 39.0, 27.0, 5.0]"]]
+    df = pd.DataFrame(data, columns=['id', 'nutrition'])
+
     actual = dataset.extract_features(df)
-    expected = pd.DataFrame([20, 269.8, 22.0, 32.0, 48.0, 39.0, 27.0]], columns = ['id', 'calories', 'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat']) 
-    
-    assert expected == actual
+    expected = pd.DataFrame(
+        [[20, 269.8, 22.0, 32.0, 48.0, 39.0, 27.0]],
+        ['id', 'calories', 'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat']
+    )
+
+    pdt.assert_frame_equal(expected, actual)
 
 
 def test_should_explode_nutrition_column_ignore_other_columns():
-    data = [
+    data = [[
         20,
         "bar",
-        "[269.8, 22.0, 32.0, 48.0, 39.0, 1337.0, 5.0]"
-    ]
-    df = pd.DataFrame(data, columns = ['id', "foo",'nutrition']) 
-    
+        "[1, 2.0, 12.0, 42.0, 3.7, 1337.0, 5.0]"
+    ]]
+    df = pd.DataFrame(data, ['id', "foo", 'nutrition'])
+
     actual = dataset.extract_features(df)
-    expected = pd.DataFrame([20, 269.8, 22.0, 32.0, 48.0, 39.0, 1337.0]], columns = ['id', 'calories', 'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat']) 
-    
-    assert expected == actual
+    expected = pd.DataFrame(
+        [[1.0, 2.0, 12.0, 42.0, 3.7, 1337.0, 5.0]],
+        ['id', 'calories', 'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat']
+    )
 
-
+    pdt.assert_frame_equal(expected, actual)
