5434ed8ff5d4084a3a000d203c061236cd31caed
Daniel Schruhl <danielschruhl@gmail.com>
2020-10-22 16:37:32 +0200

    Add integration test for normalize

--- tests/integration/test_dataset.py
+++ tests/integration/test_dataset.py
@@ -18,3 +18,28 @@ def test_should_extract_features_from_file():
     )
 
     pdt.assert_frame_equal(expected, actual)
+
+
+def test_should_normalize():
+    dataframe = pd.read_csv('tests/integration/resources/raw_recipes_dump.csv')
+    normalize_columns = ['calories', 'total_fat', 'sugar', 'sodium', 'protein', 'saturated_fat']
+
+    actual = dataset.normalize(normalize_columns, dataframe)
+    expected = pd.DataFrame(
+        [
+            [137739, 51.5 / 269.8, 0.0 / 22.0, 13.0 / 32.0, 0.0 / 48.0, 2.0 / 39.0, 0.0 / 35.0],
+            [31490, 173.4 / 269.8, 18.0 / 22.0, 0.0 / 32.0, 17.0 / 48.0, 22.0 / 39.0, 35.0 / 35.0],
+            [112140, 269.8 / 269.8, 22.0 / 22.0, 32.0 / 32.0, 48.0 / 48.0, 39.0 / 39.0, 27.0 / 35.0]
+        ],
+        columns=[
+            'id',
+            'normalized_calories',
+            'normalized_total_fat',
+            'normalized_sugar',
+            'normalized_sodium',
+            'normalized_protein',
+            'normalized_saturated_fat'
+        ]
+    )
+
+    pdt.assert_frame_equal(expected, actual)
