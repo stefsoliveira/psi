899f35f4830e240a4ee973417c026d7e405a79b6
Daniel Schruhl <danielschruhl@gmail.com>
2020-10-25 12:01:51 +0100

    Add job to create histograms for each feature

---
+++ recipes_recommendation/jobs/create_histograms.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import logging
+import sys
+
+import pandas as pd
+
+from recipes_recommendation import dataset, validation
+
+LOG = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    dataset_path = str(sys.argv[1])
+    output_path = str(sys.argv[2])
+    LOG.info("Starting job: %s", __name__)
+
+    LOG.info("Start loading features dataset from: %s", dataset_path)
+    dataframe = pd.read_csv(dataset_path)
+    LOG.info("Done loading features dataset")
+
+    LOG.info("Start creating histograms in: %s", output_path)
+    for feature_column in dataset.NUTRITION_COLUMNS:
+        LOG.info("Start creating histogram for: %s", feature_column)
+        validation.histogram(dataframe, output_path, feature_column)
+        LOG.info("Done creating histogram")
+    LOG.info("Done creating histograms")
+    LOG.info("Finishing job")
---
+++ recipes_recommendation/validation.py
@@ -0,0 +1,13 @@
+import logging
+
+import seaborn as sns
+
+LOG = logging.getLogger(__name__)
+
+
+def histogram(dataframe, output_path, feature_column):
+    LOG.info(feature_column)
+    plot = sns.histplot(data=dataframe, x=feature_column, bins=50)
+    figure = plot.get_figure()
+    figure.savefig(output_path + 'histogram_' + feature_column + '.png')
+    figure.clf()
