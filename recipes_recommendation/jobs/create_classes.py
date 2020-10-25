24d1d9342fdbf6169f2694382105754f30a05746
Daniel Schruhl <danielschruhl@gmail.com>
2020-10-25 13:58:06 +0100

    Add job to create classes

---
+++ recipes_recommendation/jobs/create_classes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import logging
+import sys
+
+import pandas as pd
+
+from recipes_recommendation import dataset, models
+
+LOG = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    dataset_path = str(sys.argv[1])
+    output_path = str(sys.argv[2])
+    LOG.info("Starting job: %s", __name__)
+
+    LOG.info("Loading dataset from: %s", dataset_path)
+    dataframe = pd.read_csv(dataset_path)
+    LOG.info("Done loading dataset")
+
+    LOG.info("Start training clustering model")
+    clustering_model = models.train_cluster_model(dataframe)
+    LOG.info("Start training clustering model")
+
+    LOG.info("Start classifying")
+    classes = clustering_model.predict(dataframe[dataset.NUTRITION_COLUMNS])
+    dataframe_with_classes = dataframe.copy()
+    dataframe_with_classes['class'] = classes
+    LOG.info("Done classifying")
+
+    LOG.info("Start saving dataset with classes to: %s", output_path)
+    dataframe_with_classes.to_csv(output_path + 'recipe_features_with_classes.csv', index=False)
+    LOG.info("Done saving dataset with classes")
+
+    LOG.info("Finishing job")
--- recipes_recommendation/models.py
+++ recipes_recommendation/models.py
@@ -1,7 +1,15 @@
+from sklearn.cluster import KMeans
 from sklearn.ensemble import IsolationForest
+
+from recipes_recommendation import dataset
 
 
 def train_outlier_classifier(dataframe, **kwargs):
     classifier = IsolationForest(**kwargs)
     classifier.fit(dataframe)
     return classifier
+
+
+def train_cluster_model(dataframe):
+    model = KMeans(n_clusters=8, random_state=0)
+    return model.fit(dataframe[dataset.NUTRITION_COLUMNS])
